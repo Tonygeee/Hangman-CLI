c3cd87bf828b670b516ee5db3986b3c124390b42
Tony Gonzalez <Tony@AntoniosMacBook.localdomain>
2018-04-10 17:28:59 -0700

    first push

---
+++ index.js
@@ -0,0 +1,59 @@
+var Word = require("./word.js");
+var inquirer = require("inquirer");
+var wordList = ["pizza", "lasagna", "burrito", "sandwhich", "pasta", "spaghetti"];
+var chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+var wordSplit = chosenWord.split("");
+var letterArray = [];
+var dashArray = [];
+var firstWord = new Word();
+var guessesLeft = 10;
+console.log("Hangman CLI Game");
+
+function initializeGame() {
+	chosenWord = wordList[Math.floor(Math.random() * wordList.length)];
+	wordSplit = chosenWord.split("");
+	firstWord.returnLetters(wordSplit, letterArray);
+	firstWord.returnWord(letterArray, dashArray)
+};
+
+function startGame() {
+	inquirer.prompt([{
+		type: "input",
+		message: "Guess a letter:",
+		name: "userInput"
+	}]).then(function(answer) {
+		firstWord.checkWord(letterArray, answer.userInput);
+		var letterInWord = false;
+		for (var i = 0; i < wordSplit.length; i++) {
+			if (answer.userInput === wordSplit[i]) {
+				letterInWord = true;
+			}
+		}
+		if (letterInWord) {
+			console.log("Correct!")
+		} else {
+			guessesLeft--;
+			console.log("Incorrect!")
+		}
+		dashArray = [];
+		firstWord.returnWord(letterArray, dashArray);
+		console.log(`${guessesLeft} guesses remaining`)
+		console.log("");
+		if (dashArray.toString() === wordSplit.toString()) {
+			console.log("Winner Winner Chicken Dinner! (PUBG?)");
+			guessesLeft = 0;
+		}
+		if (guessesLeft > 0) {
+			startGame();
+		} else {
+			console.log("You lose! Next word: ")
+			guessesLeft = 10;
+			dashArray = [];
+			letterArray = [];
+			initializeGame();
+			startGame();
+		}
+	});
+};
+initializeGame();
+startGame();
---
+++ letters.js
@@ -0,0 +1,19 @@
+function Letter(character, guessed) {
+	this.character = character; //string
+	this.guessed = guessed; //boolean
+	this.returnCharacter = function() {
+		if (this.guessed) {
+			return this.character;
+		} else {
+			return "_";
+		}
+	};
+	this.checkCharacter = function(userInput) {
+		if (userInput === this.character || this.guessed === true) {
+			this.guessed = true;
+		} else {
+			this.guessed = false;
+		}
+	}
+}
+module.exports = Letter;
